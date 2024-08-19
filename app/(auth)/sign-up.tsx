22d62e90617b99fb1351ce7fad68f1913eb6bc81
zendayat <nmapzendaya@gmail.com>
2024-08-19 13:32:47 +0400

    update: file-structue complete

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>Sign Up</Text>
+    </SafeAreaView>
+  );
+};
+export default SignUp;
---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  );
+};
+export default Onboarding;
