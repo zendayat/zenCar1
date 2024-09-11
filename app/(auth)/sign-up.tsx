b5bf7fd0976cc28d6a13d529e559de8d41e43c6e
zendayat <nmapzendaya@gmail.com>
2024-09-11 23:00:59 +0400

    beginning auth screens

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,21 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>Sign Up</Text>
-    </SafeAreaView>
+    <ScrollView className=" flex-1 bg-white">
+      <View className=" flex-1 bg-white">
+        <View className=" relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px]" />
+          <Text className=" text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+        </View>
+          <View className="p-5">
+            <InputField/>
+          </View>
+      </View>
+    </ScrollView>
   );
 };
 export default SignUp;
