a6bac38aa8d919c8c3afbc39074a5618c8c852f2
zendayat <nmapzendaya@gmail.com>
2024-09-12 13:19:46 +0400

    Note: making the or lines with self closing views.

---
+++ components/OAuth.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const OAuth = () => (
+  <View>
+    <View className=" flex flex-row justify-center items-center mt-4 gap-x-3">
+      <View className=" flex-1 h-[1px] bg-general-100" />
+      <Text className=" text-lg">Or</Text>
+      <View className=" flex-1 h-[1px] bg-general-100" />
+    </View>
+  </View>
+);
+export default OAuth;
