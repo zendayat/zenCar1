6072f58158ef253b32154bf61a2ba133bf06eec6
zendayat <nmapzendaya@gmail.com>
2024-09-12 13:34:11 +0400

    Note: placing an icon inside a custom button

--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import CustomButton from "./CustomButton";
+import { icons } from "@/constants";
 
 const OAuth = () => (
   <View>
@@ -7,6 +9,17 @@ const OAuth = () => (
       <Text className=" text-lg">Or</Text>
       <View className=" flex-1 h-[1px] bg-general-100" />
     </View>
+    <CustomButton
+      title="Log In With Google"
+      className=" mt-5 w-full shadow-none"
+      IconLeft={() => (
+        <Image
+          source={icons.google}
+          resizeMode="contain"
+          className="w-5 h-5 mx-2"
+        />
+      )}
+    />
   </View>
 );
 export default OAuth;
