716df49bf238b08da1f716a10b2c6d9621fd86f7
zendayat <nmapzendaya@gmail.com>
2024-09-12 13:51:24 +0400

    fix: link working now

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,9 +1,13 @@
+import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { Link } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
+  const onSignUpPress = async () => {};
   const [form, setForm] = useState({ name: "", email: "", password: "" });
   return (
     <ScrollView className=" flex-1 bg-white">
@@ -33,11 +37,27 @@ const SignUp = () => {
             label="Password"
             placeholder="Enter Your Password"
             icon={icons.lock}
-            secureTextEntry = {true}
-            value={form.Password}
-            onChangeText={(value) => setForm({ ...form, Password: value })}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value) => setForm({ ...form, password: value })}
           />
+          <CustomButton
+            title="Sign Up"
+            onPress={onSignUpPress}
+            className="mt-6"
+          />
+          <OAuth/>
+
+          <Link
+            href="/(auth)/sign-in"
+            className="text-lg text-center text-general-200 mt-10"
+          >
+            <Text>Already have an account?</Text>
+            <Text className=" text-primary-500">Log In</Text>
+          </Link>
         </View>
+
+        {/* Verification Modal */}
       </View>
     </ScrollView>
   );
